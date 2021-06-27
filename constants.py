82b761f723bc0f28eee3f5c9dbae7cf8cc6faa80
InconsistentBoi <85055365+InconsistentBoi@users.noreply.github.com>
2021-06-27 11:46:15 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,76 @@
+import pygame,sys,os
+from pygame.locals import *
+
+
+pygame.init()
+
+Clock=pygame.time.Clock()
+FPS= 60
+pygame.display.set_caption("Maze Duel")
+Width, Height = 1280,720
+WIN=pygame.display.set_mode((Width,Height))
+
+font=pygame.font.SysFont(None,30)
+
+BLACK= (0,0,0) 
+WHITE = (255,255,255) 
+RED= (255,0,0)
+BLUE=(0,0,255)
+
+click=False
+
+normal_size=(300,100)
+enlarged_size=(380,120)
+
+Background_image_unrefined= pygame.image.load(os.path.join("images","Main_Menu_Images","Background.png"))
+Background=pygame.transform.scale(Background_image_unrefined,(Width,Height))
+
+Blank_BG_unrefined= pygame.image.load(os.path.join("images","Main_Menu_Images","Blank_BG.png"))
+Blank_BG=pygame.transform.scale(Blank_BG_unrefined,(Width,Height))
+
+Button_play_unrefined=pygame.image.load(os.path.join("images","Main_Menu_Images","Button_Play.png"))
+Image_play=pygame.transform.scale(Button_play_unrefined,(normal_size))
+Image_play_enlarged=pygame.transform.scale(Button_play_unrefined,(enlarged_size))
+
+Button_options_unrefined=pygame.image.load(os.path.join("images","Main_Menu_Images","Button_Options.png"))
+Image_options=pygame.transform.scale(Button_options_unrefined,(normal_size))
+Image_options_enlarged=pygame.transform.scale(Button_options_unrefined,(enlarged_size))
+
+Button_exit_unrefined=pygame.image.load(os.path.join("images","Main_Menu_Images","Button_Exit.png"))
+Image_exit=pygame.transform.scale(Button_exit_unrefined,(normal_size))
+Image_exit_enlarged=pygame.transform.scale(Button_exit_unrefined,(enlarged_size))
+
+Button_fullscreen_unrefined=pygame.image.load(os.path.join("images","Main_Menu_Images","Button_Fullscreen.png"))
+Image_fullscreen=pygame.transform.scale(Button_fullscreen_unrefined,(normal_size))
+Image_fullscreen_enlarged=pygame.transform.scale(Button_fullscreen_unrefined,(enlarged_size))
+
+Button_login_unrefined=pygame.image.load(os.path.join("images","Main_Menu_Images","Button_Login.png"))
+Image_login=pygame.transform.scale(Button_login_unrefined,(normal_size))
+Image_login_enlarged=pygame.transform.scale(Button_login_unrefined,(enlarged_size))
+
+Button_back_unrefined=pygame.image.load(os.path.join("images","Main_Menu_Images","Button_Back.png"))
+Image_back=pygame.transform.scale(Button_back_unrefined,(180,80))
+Image_back_enlarged=pygame.transform.scale(Button_back_unrefined,(190,90))
+
+
+Button_bg_unrefined=pygame.image.load(os.path.join("images","Text_Bg.png"))
+Button_bg=pygame.transform.scale(Button_bg_unrefined,(220,80))
+
+base_font = pygame.font.Font(None, 45)
+user_text = ''
+
+input_rect = pygame.Rect(200,300,335,45)
+rectcol_a = pygame.Color('white')              #active
+rectcol_p = pygame.Color('lightskyblue3')      #passive
+colour = rectcol_p   
+active = False
+charcount = 0
+
+pbase_font = pygame.font.Font(None, 40)
+puser_text = ''
+prectcol_a = pygame.Color('white')              #active
+prectcol_p = pygame.Color('lightskyblue3')      #passive
+pcolour = prectcol_p   
+pinput_rect = pygame.Rect(200,600,335,45)   
+pactive = False
+pcharcount = 0
---
+++ login.py
@@ -0,0 +1,43 @@
+import pygame,sys,os
+from pygame.locals import *
+from Functions_Constants import constants , Transition_moving, mfunc
+
+def name_input(event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if constants.input_rect.collidepoint(event.pos):
+            print(event.pos)
+            constants.active = True
+        else:
+            constants.active = False
+
+            
+    if event.type==pygame.KEYDOWN:  
+        if constants.active == True:
+            if event.key==pygame.K_BACKSPACE:
+                constants.charcount-=1
+                constants.user_text = constants.user_text[:-1]
+                constants.active = True
+            else:
+                if constants.charcount <= 10:
+                    constants.user_text += event.unicode 
+                    constants.charcount+=1
+
+def pword_input(event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if constants.pinput_rect.collidepoint(event.pos):
+            print(event.pos)
+            constants.pactive = True
+        else:
+            constants.pactive = False
+
+            
+    if event.type==pygame.KEYDOWN:  
+        if constants.pactive == True:
+            if event.key==pygame.K_BACKSPACE:
+                constants.pcharcount-=1
+                constants.puser_text = constants.puser_text[:-1]
+                constants.pactive = True
+            else:
+                if constants.pcharcount <= 15:
+                    constants.puser_text += event.unicode 
+                    constants.pcharcount+=1
